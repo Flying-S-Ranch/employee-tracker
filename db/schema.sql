28e1f65aa571592fa580f96e1e7d66e7a2956147
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-21 15:58:59 -0500

    adds tables to schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -10,3 +10,24 @@ CREATE TABLE department (
     id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
+
+CREATE TABLE role (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    dept_id INT,
+    FOREIGN KEY (dept_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
+
+CREATE TABLE employee (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL
+)
