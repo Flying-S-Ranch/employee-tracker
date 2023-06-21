307a850212a802fdd028bcb533c91613fb32bb03
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-21 16:09:02 -0500

    fills in department and role tables

---
+++ db/seeds.sql
@@ -0,0 +1,15 @@
+INSERT INTO department (name)
+VALUES  ("Sales"),
+        ("Research"),
+        ("Accounting"),
+        ("Production");
+
+INSERT INTO role (title, salary, dept_id)
+VALUES  ("Sales Rep", 50000, 1),
+        ("Assistant to Sales Rep", 21000, 1),
+        ("Lab Technician", 25000, 2),
+        ("Senior Scientist", 75000, 2),
+        ("Accountant", 50000, 3),
+        ("Accounting Manager", 65000, 3),
+        ("Production Coordinator", 45000, 4),
+        ("Production Assistant", 22000, 4);
