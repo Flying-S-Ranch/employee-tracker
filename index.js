78cf0eab565881761e2ccbbc1751bcf211eb48c6
Samantha <131906442+Flying-S-Ranch@users.noreply.github.com>
2023-06-21 22:22:50 -0500

    allows users to add a department

--- index.js
+++ index.js
@@ -16,6 +16,11 @@ const questions = [
         message: 'What would you like to do?',
         name: 'main',
         choices: ['view all departments', 'view all roles', 'view all employees', 'add a department', 'add a role', 'add an employee', 'update an employee role']
+    },
+    {
+        type: 'input',
+        message: 'What department would you like to add?',
+        name: 'newDept'
     }
 ]
 
@@ -40,10 +45,21 @@ const displayChoice = (response) => {
         JOIN role ON role.id = employee.role_id`, function (err, results) {
             console.table(results)
         })
+    } else if (answerString === '{"main":"add a department"}') {
+        function addDept({newDept}) {
+            db.query(`INSERT INTO department (name)
+            VALUES ("${newDept}")`)
+            db.query('SELECT * FROM department', function (err, results) {
+                console.table(results)
+            })
+        }
+        inquirer
+          .prompt(questions[1])
+          .then(addDept)
     }
     
 }
 
 inquirer
-  .prompt(questions)
+  .prompt(questions[0])
   .then((displayChoice));
